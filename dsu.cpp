d2ea651b4e67a0e4a3ab0a3e986bc791148c05f8
Navin <navinmech10@gmail.com>
2024-10-26 20:59:44 +0530

    dsu_template_unweighted

---
+++ dsu.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define mp make_pair
+#define inf LLONG_MAX
+using namespace std;
+using ll = long long;
+using pll = pair<long long, long long>;
+const int mod = 1e9+7;
+
+struct DSU {
+    vector<int> parent, size;
+    DSU(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (size[rootX] < size[rootY]) swap(rootX, rootY);
+            size[rootX] += size[rootY];
+            parent[rootY] = rootX;
+        }
+    }
+};
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    solve();
+
+    return 0;
+}
